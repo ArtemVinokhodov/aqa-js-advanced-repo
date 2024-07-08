958a51a48eed488d17e0606469ab083114c90ee0
Artem_Vinokhodov <a.vinohod@gmail.com>
2024-07-08 13:38:16 +0200

    home task primitive types

---
+++ Task4/jsBasics/task4.js
@@ -0,0 +1,25 @@
+function calculateCircleArea() {
+    let radius = 5;
+    let circleArea = Math.PI * Math.pow(radius, 2);
+    console.log(`Circle area: ${circleArea.toFixed(2)}`);
+}
+
+calculateCircleArea();
+
+function calculateRectangleArea() {
+    let length = 10;
+    let width = 7;
+    let rectangleArea = length * width;
+    console.log(`Rectangle area: ${rectangleArea}`);
+}
+
+calculateRectangleArea();
+
+function calculateCylinderVolume() {
+    let radius = 4;
+    let height = 10;
+    let cylinderVolume = Math.PI * Math.pow(radius, 2) * height;
+    console.log(`Cylinder volume: ${cylinderVolume.toFixed(2)}`);
+}
+
+calculateCylinderVolume();
